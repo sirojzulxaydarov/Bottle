99f4f5970c3d8c6e8ac74ed4e20dd3fb1023fe70
XR <xrustam0510@gmail.com>
2024-02-03 21:30:14 +0500

    add water

--- src/uz/pdp/Main.java
+++ src/uz/pdp/Main.java
@@ -1,8 +1,29 @@
 package uz.pdp;
 
+import uz.pdp.entity.Bottle;
+import uz.pdp.util.Input;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Salom Dunyo");
+        Bottle bottle = new Bottle(
+               Input.inputSTR("Enter bottle name: "),
+               Input.inputINT("Enter bottle capacity")
+        );
+        while (true) {
+            displayMenu();
+            switch (Input.inputINT("Choose: ")) {
+                case 1 -> bottle.addWater();
+//                case 2 -> bottle.pourWater();
+            }
+        }
 
+    }
+
+    private static void displayMenu() {
+        System.out.println("""
+                1 - add water
+                2 - pour water
+                0 - exit
+                """);
     }
 }
