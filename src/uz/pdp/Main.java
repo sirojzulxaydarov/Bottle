e43ae9085fa2c2ba0a0a19b819d16848747c6e9d
XR <xrustam0510@gmail.com>
2024-02-03 21:59:23 +0500

    add water

--- src/uz/pdp/Main.java
+++ src/uz/pdp/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
         Bottle bottle = new Bottle(
                Input.inputSTR("Enter bottle name: "),
-               Input.inputINT("Enter bottle capacity")
+               Input.inputINT("Enter bottle capacity: ")
         );
         while (true) {
             displayMenu();
